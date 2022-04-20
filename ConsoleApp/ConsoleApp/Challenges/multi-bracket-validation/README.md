e689954195c700c9fc3fc5f1d705656bd1232f33
Hanan-Nathem-Saadeh <hanan.nathem111@gmail.com>
2022-04-20 22:27:19 +0300

    stack-queue-brackets

---
+++ ConsoleApp/ConsoleApp/Challenges/multi-bracket-validation/README.md
@@ -0,0 +1,26 @@
+﻿
+# Multi-bracket Validation
+
+## Challenge Summary
+Write a function which accepts a string containing a pattern of brackets and returns true if the pattern of brackets is correct. Otherwise it returns false.
+- Your function should take a string as its only argument.
+- should return a boolean representing whether or not the brackets in the string are balanced.
+
+**There are 3 types of brackets:**
+
+- Round Brackets : ()
+- Square Brackets : []
+- Curly Brackets : {}
+
+
+# Whiteboard Process
+
+![](../../img/validatebrackets.png)
+
+# Approach & Efficiency
+Time Complexity  O(n)
+
+# Test
+![](../../img/BracketUnitTest.png)
+
+
---
+++ ConsoleApp/ConsoleApp/Challenges/multi-bracket-validation/multi-bracket-validation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using stack_and_queue.Classes;
+namespace ConsoleApp.Challenges.multi_bracket_validation
+{
+    public class multi_bracket_validation
+    {
+        public static bool multiBracketValidation(String StringInput)
+        {
+            Stack MyStack = new Stack();
+            if (StringInput == null)
+                return true;
+            for (int i = 0; i < StringInput.Length; i++)
+            {
+
+                char temp = StringInput[i];
+                if (temp=='('||temp=='{'||temp=='[')
+                {
+                    MyStack.Push(temp);
+                }
+                if (temp == ')' || temp == '}' || temp == ']')
+                {
+                    if (MyStack.IsEmpty())
+                    {
+                        return false;
+                    }
+                    char temp2 = (char)MyStack.peek();
+                    if (temp == '}' && temp2 == '{' || temp == ']' && temp2 == '[' || temp == ')' && temp2 == '(')
+                    {
+                        MyStack.Pop();
+                    }
+                    else 
+                        return false;
+                }
+            }
+            bool Result = MyStack.IsEmpty();
+            return Result;
+        }
+    }
+}
