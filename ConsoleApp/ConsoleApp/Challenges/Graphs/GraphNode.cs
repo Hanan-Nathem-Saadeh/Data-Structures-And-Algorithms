0e01cd37655804639918f857ae41e6c5bc134dc6
Hanan-Nathem-Saadeh <hanan.nathem111@gmail.com>
2022-06-21 07:03:33 +0300

    Graph

---
+++ ConsoleApp/ConsoleApp/Challenges/Graphs/GraphNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Challenges.Graphs
+{
+    public  class GraphNode
+    {
+        public int Value { get; set; }
+        public List<GraphNode> Neighbors { get; set; }
+
+        public GraphNode(int Value)
+        {
+            Value = Value;
+        }
+    }
+}
